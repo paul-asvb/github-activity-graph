02e61993701c2c50354680ad78cc3202b970811a
Paul Mayer <paul@paul-mayer.eu>
2021-11-21 22:30:59 +0100

    Init d3

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,66 @@
 <script>
-	export let name;
+  import * as d3 from "d3";
+
+  const padding = 0.05;
+  const grid_row_count = 7;
+
+  const view_width = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+
+  const view_height = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
+
+  const element_width = view_width / grid_row_count;
+  const relative_parring = view_width * padding;
+
+  function execute(ingredient) {
+    console.log("Just executed:", ingredient);
+  }
+
+  let test = Array(1000)
+    .fill(0)
+    .map((e) => d3.randomInt(1, 10)());
+
+  console.log(test);
+
+  //Create SVG element
+  let svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", view_width)
+    .attr("height", view_height);
+
+  //   svg // Select the body
+  //     .selectAll("div") // Select all the p tags
+  //     .data(test) // Bind our data
+  //     .enter() // Grab our 'new' data points
+  //     .append("div") // Add a paragraph for each
+  //     .html((d) => d);
+
+  let g = svg
+    .append("g")
+    .attr(
+      "transform",
+      `translate(${relative_parring}px, ${relative_parring}px)`
+    );
+
+  g.selectAll("rects")
+    .data(test)
+    .enter()
+    .append("rect")
+    .attr("x", (_v, index) => {
+      let grid_mod = index % grid_row_count;
+      return view_width - element_width - element_width * grid_mod;
+    })
+    .attr("y", (_v, index) => {
+      let grid_mod = index % grid_row_count;
+      return view_width - element_width - element_width * grid_mod;
+    })
+    .attr("width", element_width)
+    .attr("height", element_width)
+    .attr("fill", "green");
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
