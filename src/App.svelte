0a7c7db23aa5405785f8e9879c50c1e3a1deb052
Paul Mayer <paul@paul-mayer.eu>
2021-11-22 18:06:03 +0100

    remove d3, use svelte only

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,125 @@
 <script>
-  import * as d3 from "d3";
-
-  const padding = 0.05;
-  const grid_row_count = 7;
-
-  const view_width = Math.max(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-
-  const view_height = Math.max(
-    document.documentElement.clientHeight || 0,
-    window.innerHeight || 0
-  );
-
-  const element_width = view_width / grid_row_count;
-  const relative_parring = view_width * padding;
-
-  function execute(ingredient) {
-    console.log("Just executed:", ingredient);
-  }
-
-  let test = Array(1000)
-    .fill(0)
-    .map((e) => d3.randomInt(1, 10)());
-
-  console.log(test);
-
-  //Create SVG element
-  let svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", view_width)
-    .attr("height", view_height);
-
-  //   svg // Select the body
-  //     .selectAll("div") // Select all the p tags
-  //     .data(test) // Bind our data
-  //     .enter() // Grab our 'new' data points
-  //     .append("div") // Add a paragraph for each
-  //     .html((d) => d);
-
-  let g = svg
-    .append("g")
-    .attr(
-      "transform",
-      `translate(${relative_parring}px, ${relative_parring}px)`
+  import Week from "./Week.svelte";
+  import { setContext } from "svelte";
+  let padding = 16;
+  let width = () =>
+    Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  let height = () =>
+    Math.max(
+      document.documentElement.clientHeight || 0,
+      window.innerHeight || 0
     );
 
-  g.selectAll("rects")
-    .data(test)
-    .enter()
-    .append("rect")
-    .attr("x", (_v, index) => {
-      let grid_mod = index % grid_row_count;
-      return view_width - element_width - element_width * grid_mod;
-    })
-    .attr("y", (_v, index) => {
-      let grid_mod = index % grid_row_count;
-      return view_width - element_width - element_width * grid_mod;
-    })
-    .attr("width", element_width)
-    .attr("height", element_width)
-    .attr("fill", "green");
+  setContext("view", {
+    width,
+    height,
+    padding,
+    col_number: 7,
+  });
+
+  let weeks = Array(52)
+    .fill(0)
+    .map((e, i) => {
+      return {
+        data: "id_" + i,
+        days: Array(7)
+          .fill(0)
+          .map((e) => Math.floor(Math.random() * 10)),
+      };
+    });
+
+  // const padding = 0.05;
+  // const grid_row_count = 7;
+
+  // const view_width = Math.max(
+  //   document.documentElement.clientWidth || 0,
+  //   window.innerWidth || 0
+  // );
+
+  // const view_height = Math.max(
+  //   document.documentElement.clientHeight || 0,
+  //   window.innerHeight || 0
+  // );
+
+  // const element_width = view_width / grid_row_count;
+  // const relative_parring = view_width * padding;
+
+  // function execute(ingredient) {
+  //   console.log("Just executed:", ingredient);
+  // }
+
+  // let test = Array(52)
+  //   .fill(0)
+  //   .map((e, i) => {
+  //     return {
+  //       data: "id_" + i,
+  //       days: Array(7)
+  //         .fill(0)
+  //         .map((e) => d3.randomInt(1, 10)()),
+  //     };
+  //   });
+
+  // console.log(test);
+
+  // //Create SVG element
+  // let svg = d3
+  //   .select("body")
+  //   .append("svg")
+  //   .attr("width", view_width)
+  //   .attr("height", view_height);
+
+  // //   svg // Select the body
+  // //     .selectAll("div") // Select all the p tags
+  // //     .data(test) // Bind our data
+  // //     .enter() // Grab our 'new' data points
+  // //     .append("div") // Add a paragraph for each
+  // //     .html((d) => d);
+
+  // let g = svg
+  //   .append("g")
+  //   .attr("transform", `translate(${relative_parring} ${relative_parring})`);
+
+  // let week = g
+  //   .selectAll(null)
+  //   .data(test)
+  //   .enter()
+  //   .append("g")
+  //   .attr("transform", (_v, i) => `translate(0 ${i * element_width})`);
+
+  // week
+  //   .insert("rect")
+  //   .attr("x", (v, index) => {
+  //     console.log(v);
+  //     let grid_mod = index % grid_row_count;
+  //     return view_width - element_width - element_width * grid_mod;
+  //   })
+  //   .attr("y", 0)
+  //   .attr("width", element_width)
+  //   .attr("height", element_width)
+  //   .attr("fill", "green");
+
+  // g.selectAll(null)
+  // .data(test)
+  // .enter()
+  // .append("g")
+  // .attr("x", (_v, index) => {
+  //   let grid_mod = index % grid_row_count;
+  //   return view_width - element_width - element_width * grid_mod;
+  // })
+  // .attr("y", (_v, index) => {
+  //   let grid_mod = index % grid_row_count;
+  //   //return view_width - element_width - element_width * grid_mod;
+  //   return "23";
+  // })
+  // .attr("width", element_width)
+  // .attr("height", element_width)
+  // .attr("fill", "green");
 </script>
+
+<svg width={width()} height="640"
+  ><g transform="translate({padding} {padding})">
+    {#each weeks as week, i}
+      <Week index={i} days={week.days} />
+    {/each}
+  </g>
+</svg>
---
+++ src/Week.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let days = [];
+  export let index = 0;
+  import { getContext } from "svelte";
+
+  const view = getContext("view");
+  console.log(view, days, index);
+
+  const element_width =
+    (view.width() - view.padding * view.col_number - view.padding) /
+    view.col_number;
+</script>
+
+<g transform="translate(0 {index * (element_width + view.padding)})">
+  {#each days as day, i}
+    <rect
+      x={i * element_width + view.padding * i}
+      y="0"
+      width={element_width}
+      height={element_width}
+      opacity={day / 10}
+      rx={element_width * 0.1}
+      fill="green"
+    />
+  {/each}
+</g>
